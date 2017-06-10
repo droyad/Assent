3ce5497c95fcf1c2872fd933dc63755ea7d3f567
Adam Chester <adamchester@gmail.com>
2017-06-10 14:23:04 +1000

    Added `VsCodeDiffProgram` w/ OSX support

---
+++ src/Assent/Reporters/DiffPrograms/VsCodeDiffProgram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assent.Reporters.DiffPrograms
+{
+    public class VsCodeDiffProgram : DiffProgramBase
+    {
+        static VsCodeDiffProgram()
+        {
+            var paths = new List<string>();
+            if (DiffReporter.IsWindows)
+            {
+                var programFiles = Environment.GetEnvironmentVariable("ProgramFiles");
+                var x86 = Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+                paths.Add($@"{programFiles}\Microsoft VS Code\Code.exe");
+                if (!String.IsNullOrEmpty(x86))
+                {
+                    paths.Add($@"{x86}\Microsoft VS Code\Code.exe");
+                }
+            }
+            else
+            {
+                paths.Add("/usr/local/bin/code");
+            }
+            DefaultSearchPaths = paths;
+        }
+
+        public static readonly IReadOnlyList<string> DefaultSearchPaths;
+
+
+        public VsCodeDiffProgram() : base(DefaultSearchPaths)
+        {
+        }
+
+        public VsCodeDiffProgram(IReadOnlyList<string> searchPaths)
+            : base(searchPaths)
+        {
+        }
+
+        protected override string CreateProcessStartArgs(string receivedFile, string approvedFile)
+        {
+            return $"--diff --wait --new-window \"{receivedFile}\" \"{approvedFile}\"";
+        }
+    }
+}
