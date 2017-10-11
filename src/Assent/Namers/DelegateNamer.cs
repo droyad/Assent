ef48035890406923de7eacc8eda742628ec99342
Asad Saeeduddin <masaeedu@gmail.com>
2017-10-11 12:39:19 -0400

    Add delegate-accepting UsingNamer overload

---
+++ src/Assent/Namers/DelegateNamer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assent.Namers
+{
+    class DelegateNamer : INamer
+    {
+        readonly Func<TestMetadata, string> _f;
+
+        public DelegateNamer(Func<TestMetadata, string> f)
+        {
+            _f = f;
+        }
+
+        public string GetName(TestMetadata metadata)
+        {
+            return _f(metadata);
+        }
+    }
+}
