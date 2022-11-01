3992e5f7face0bafff296115c00181a28db83258
Roman Belonohy <r.belonohy@quadient.com>
2022-11-01 09:18:25 +0100

    Added Meld diff application as a diff program

---
+++ src/Assent/Reporters/DiffPrograms/MeldDiffProgram.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assent.Reporters.DiffPrograms;
+
+public class MeldDiffProgram : DiffProgramBase
+{
+    static readonly IReadOnlyList<string> DefaultSearchPaths;
+
+    static MeldDiffProgram()
+    {
+        var paths = new List<string>();
+        if (DiffReporter.IsWindows)
+        {
+            paths.AddRange(WindowsProgramFilePaths
+                .Select(p => $@"{p}\Meld\Meld.exe")
+                .ToArray());
+        }
+        else
+        {
+            paths.Add("/usr/bin/meld");
+            paths.Add("/usr/local/bin/meld");
+            paths.Add("/snap/bin/meld");
+            paths.Add("/opt/homebrew/bin/meld");
+        }
+        DefaultSearchPaths = paths;
+    }
+
+    public MeldDiffProgram()
+        : base(DefaultSearchPaths) { }
+
+    protected override string CreateProcessStartArgs(string receivedFile, string approvedFile) => "\"" + receivedFile + "\" \"" + approvedFile + "\"";
+}
+
